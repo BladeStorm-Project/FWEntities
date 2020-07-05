addd8847b27c10d59f808dfcf099985f4ce18f26
Samuel Pozzobon <42522736+SamuelPozzobon@users.noreply.github.com>
2020-07-05 15:15:15 +0200

    Update DustShopTask.php

--- src/fwentities/task/DustShopTask.php
+++ src/fwentities/task/DustShopTask.php
@@ -6,6 +6,13 @@ use fwentities\{Main, entities\DustShop};
 use pocketmine\{Server, Player, utils\TextFormat as Color, entity\Effect, entity\EffectInstance, scheduler\Task};
 
 class DustShopTask extends Task {
+    
+    public $plugin;
+
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+    }
+    
     public function onRun(int $currentTick) : void {
         foreach (Server::getInstance()->getDefaultLevel()->getEntities() as $entity) {
             if ($entity instanceof DustShop) {
