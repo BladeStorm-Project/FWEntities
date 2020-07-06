a0de5c0a445ae954401c9bfe7c439f26f32b9b6b
Samuel Pozzobon <42522736+SamuelPozzobon@users.noreply.github.com>
2020-07-06 13:23:04 +0200

    Reverting to spawn player's skin instead of custom geometry

--- src/fwentities/entities/EntityManager.php
+++ src/fwentities/entities/EntityManager.php
@@ -6,31 +6,11 @@ use pocketmine\{Server, Player, utils\TextFormat as Color, level\Level, entity\E
 
 final class EntityManager {
 
-    public function setSkins($player, string $value) {
-        $dir = Main::getInstance()->getDataFolder() . $value . '.png';
-        $load = @imagecreatefrompng($dir);
-        $skinbytes = '';
-        $values = (int)@getimagesize($dir)[1];
-        for($y = 0; $y < $values; $y++) {
-            for($x = 0; $x < 64; $x++) {
-                $bytes = @imagecolorat($load, $x, $y);
-                $a = ((~((int)($bytes >> 24))) << 1) & 0xff;
-                $b = ($bytes >> 16) & 0xff;
-                $c = ($bytes >> 8) & 0xff;
-                $d = $bytes & 0xff;
-                $skinbytes .= chr($b) . chr($c) . chr($d) . chr($a);
-            }
-        }
-        @imagedestroy($load);
-        $player->setSkin(new Skin($player->getSkin()->getSkinId(), $skinbytes, '', 'geometry.' . $value,file_get_contents(Main::getInstance()->getDataFolder() . $value. '.json')));
-        $player->sendSkin();
-    }
-
     public function setTitle(Player $player) {
         $nbt = Entity::createBaseNBT(new Vector3((float)$player->getX(), (float)$player->getY(), (float)$player->getZ()));
         $nbt->setTag(clone $player->namedtag->getCompoundTag('Skin'));
         $human = new Title($player->getLevel(), $nbt);
-        $this->setSkins($human, 'Title');
+        $human->setSkin(new Skin($player->getSkin()->getSkinId(), $human));
         $human->setNameTagVisible(true);
         $human->setNameTagAlwaysVisible(true);
         $human->yaw = $player->getYaw();
@@ -42,7 +22,7 @@ final class EntityManager {
         $nbt = Entity::createBaseNBT(new Vector3((float)$player->getX(), (float)$player->getY(), (float)$player->getZ()));
         $nbt->setTag(clone $player->namedtag->getCompoundTag('Skin'));
         $human = new Locker($player->getLevel(), $nbt);
-        $this->setSkins($human, 'Locker');
+        $human->setSkin(new Skin($player->getSkin()->getSkinId(), $human));
         $human->setNameTagVisible(true);
         $human->setNameTagAlwaysVisible(true);
         $human->yaw = $player->getYaw();
@@ -54,7 +34,7 @@ final class EntityManager {
         $nbt = Entity::createBaseNBT(new Vector3((float)$player->getX(), (float)$player->getY(), (float)$player->getZ()));
         $nbt->setTag(clone $player->namedtag->getCompoundTag('Skin'));
         $human = new DustShop($player->getLevel(), $nbt);
-        $this->setSkins($human, 'DustShop');
+        $human->setSkin(new Skin($player->getSkin()->getSkinId(), $human));
         $human->setNameTagVisible(true);
         $human->setNameTagAlwaysVisible(true);
         $human->yaw = $player->getYaw();
@@ -66,7 +46,7 @@ final class EntityManager {
         $nbt = Entity::createBaseNBT(new Vector3((float)$player->getX(), (float)$player->getY(), (float)$player->getZ()));
         $nbt->setTag(clone $player->namedtag->getCompoundTag('Skin'));
         $human = new Booster($player->getLevel(), $nbt);
-        $this->setSkins($human, 'Booster');
+        $human->setSkin(new Skin($player->getSkin()->getSkinId(), $human));
         $human->setNameTagVisible(true);
         $human->setNameTagAlwaysVisible(true);
         $human->yaw = $player->getYaw();
